70dc72c0145c9b438af872e816de90bd198f892a
Jinhao Hu <hjinhao066@gmail.com>
2022-10-24 15:33:31 +0800

    new

--- app-demo.py
+++ app-demo.py
@@ -1,23 +1,46 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use('seaborn')
 
-# create datadrame
-df = pd.DataFrame({
-    'name':['John', 'Mary', 'Tom'],
-    'gender':['male', 'female', 'male'],
-    'age':[20, 22, 21],
-})
 
-# set title
-st.title('first app')
+st.title('World Cites')
+df = pd.read_csv('worldcities.csv')
+
+# note that you have to use 0.0 and 40.0 given that the data type of population is float
+population_filter = st.slider('Minimal Population (Millions):', 0.0, 40.0, 3.6)  # min, max, default
+
+# create a multi select
+capital_filter = st.sidebar.multiselect(
+     'Capital Selector',
+     df.capital.unique(),  # options
+     df.capital.unique())  # defaults
+
+# create a input form
+form = st.sidebar.form("country_form")
+country_filter = form.text_input('Country Name (enter ALL to reset)', 'ALL')
+form.form_submit_button("Apply")
+
+
+# filter by population
+df = df[df.population >= population_filter]
+
+# filter by capital
+df = df[df.capital.isin(capital_filter)]
+
+if country_filter!='ALL':
+    df = df[df.country == country_filter]
+
+# show on map
+st.map(df)
 
 # show dataframe
-st.write(df)
+st.subheader('City Details:')
+st.write(df[['city', 'country', 'population']])
 
-# show a plot
-
-fig, ax = plt.subplots()
-df.age.plot(ax=ax)
+# show the plot
+st.subheader('Total Population By Country')
+fig, ax = plt.subplots(figsize=(20, 5))
+pop_sum = df.groupby('country')['population'].sum()
+pop_sum.plot.bar(ax=ax)
 st.pyplot(fig)
-print(5)
